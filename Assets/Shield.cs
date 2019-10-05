8300bdc2186e5fbcba77ef77d4d0528085488737
Azim Momin <azimmomin6@gmail.com>
2019-10-05 00:11:04 -0700

    Gameobject now follows mouse.

---
+++ Assets/Shield.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [SerializeField] private Transform shieldTransform = null;
+    [SerializeField] private Camera mainCamera = null;
+    private Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+    void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        if (mousePosition.x < 0 || mousePosition.x > screenRect.width || mousePosition.y < 0 || mousePosition.y > screenRect.height)
+        {
+            return;
+        }
+
+        mousePosition.z = shieldTransform.position.z - mainCamera.transform.position.z;
+        shieldTransform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+    }
+}
