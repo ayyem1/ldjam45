3c4089b9b400471b77efa8bfa3508ddf0d400208
Azim Momin <azimmomin6@gmail.com>
2019-10-05 13:09:33 -0700

    Addding thread safe game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public sealed class GameManager : MonoBehaviour
+{
+    private static GameManager instance = null;
+    [SerializeField] public uint remainingSentryAmmo = 0;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                GameManager[] instances = FindObjectsOfType<GameManager>();
+                if (instances.Length == 0)
+                {
+                    throw new System.Exception("ERROR: Could not find an instance of the GameManager in the current scene. Please attach one to a gameobject.");
+                }
+                else if (instances.Length > 1)
+                {
+                    throw new System.Exception("ERROR: Found multiple instances of the gamemanager in the current scene. Count=" + instances.Length);
+                }
+
+                instance = instances[0];
+            }
+
+            return instance;
+        }
+    }
+
+    static GameManager() { }
+
+    private GameManager() { }
+}
---
+++ Assets/Scripts/Sentry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+public class Sentry : MonoBehaviour
+{
+    [SerializeField] private Transform sentryTransform = null;
+    [SerializeField] private Camera mainCamera = null;
+    [SerializeField] private float sentryRadius = 0.0F;
+    [SerializeField] private Vector3 sentryCenter = Vector3.zero;
+
+    private Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
+
+    private void Update()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = sentryTransform.position.z - mainCamera.transform.position.z;
+        mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+        Vector3 vectorToMouse = mousePosition - sentryCenter;
+        // Clamp the sentry arc to the top half of the circle.
+        // This translates to all y coordinates greater than
+        // the center
+        vectorToMouse.y = Mathf.Max(sentryCenter.y, vectorToMouse.y);
+        // This addition is so that the sentry travels the whole
+        // path to the other side of the semi-circle instead of
+        // jumping there.
+        vectorToMouse.y += 1.0F;
+        float sqrmag = vectorToMouse.sqrMagnitude;
+        float magnitude = (float) Math.Sqrt(sqrmag);
+        float normalizedX = vectorToMouse.x / magnitude;
+        float normalizedY = vectorToMouse.y / magnitude;
+        vectorToMouse.x = normalizedX * sentryRadius;
+        vectorToMouse.y = normalizedY * sentryRadius;
+
+        sentryTransform.position = sentryCenter + vectorToMouse;
+    }
+}
