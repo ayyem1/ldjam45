ce51169e95aba0c1e524b1284e572abea98216c3
Azim Momin <azimmomin6@gmail.com>
2019-10-05 02:29:44 -0700

    Shield now moves in a semi-circular path.

--- Assets/Shield.cs
+++ Assets/Shield.cs
@@ -1,20 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Shield : MonoBehaviour
 {
     [SerializeField] private Transform shieldTransform = null;
     [SerializeField] private Camera mainCamera = null;
+    [SerializeField] private float shieldRadius = 0.0F;
+    [SerializeField] private Vector3 shieldCenter = Vector3.zero;
+
     private Rect screenRect = new Rect(0, 0, Screen.width, Screen.height);
 
-    void Update()
+    private void Update()
     {
         Vector3 mousePosition = Input.mousePosition;
-        if (mousePosition.x < 0 || mousePosition.x > screenRect.width || mousePosition.y < 0 || mousePosition.y > screenRect.height)
-        {
-            return;
-        }
-
         mousePosition.z = shieldTransform.position.z - mainCamera.transform.position.z;
-        shieldTransform.position = mainCamera.ScreenToWorldPoint(mousePosition);
+        mousePosition = mainCamera.ScreenToWorldPoint(mousePosition);
+
+        Vector3 vectorToMouse = mousePosition - shieldCenter;
+        // Clamp the shield arc to the top half of the circle.
+        // This translates to all y coordinates greater than
+        // the center
+        vectorToMouse.y = Mathf.Max(shieldCenter.y, vectorToMouse.y);
+        // This addition is so that the shield travels the whole
+        // path to the other side of the semi-circle instead of
+        // jumping there.
+        vectorToMouse.y += 1.0F;
+        float sqrmag = vectorToMouse.sqrMagnitude;
+        float magnitude = (float) Math.Sqrt(sqrmag);
+        float normalizedX = vectorToMouse.x / magnitude;
+        float normalizedY = vectorToMouse.y / magnitude;
+        vectorToMouse.x = normalizedX * shieldRadius;
+        vectorToMouse.y = normalizedY * shieldRadius;
+
+        shieldTransform.position = shieldCenter + vectorToMouse;
     }
 }
