726d18cc4598f2077011747aebd5c464cd34156f
jslawski <jslawski@umich.edu>
2019-10-06 02:28:31 -0400

    Changes to Difficulty scriptableobject

--- Assets/Scripts/Difficulty.cs
+++ Assets/Scripts/Difficulty.cs
@@ -7,6 +7,10 @@ public class Difficulty : ScriptableObject
     public float maxSecondsBetweenSpawns;
     [Range(0.0F, 100.0F)] public float minChanceToSpawnSuccessfully;
     [Range(0.0F, 100.0F)] public float maxChanceToSpawnSuccessfully;
+    [Range(0.0F, 100.0F)] public float minChanceToSpawnElite;
+    [Range(0.0F, 100.0F)] public float maxChanceToSpawnElite;
+    public float minTemptationSpeed;
+    public float maxTemptationSpeed;
     public uint ammoReductionForMiss;
     public uint ammoGrantForHit;
     public int breathBPM;
