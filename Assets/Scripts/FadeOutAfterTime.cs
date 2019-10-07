df415283987891cfcbe675093cb7942afb2d7b5c
jslawski <jslawski@umich.edu>
2019-10-07 09:50:32 -0400

    UI stuff, splash screen

---
+++ Assets/Scripts/FadeOutAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//No, this doesn't actually fade.  Yes I am sorry.
+//It is 6:36 and I am dying
+public class FadeOutAfterTime : MonoBehaviour
+{
+    public float timeBeforeFade;
+    public GameObject fadingImage;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        StartCoroutine(this.FadeOutImage());
+    }
+
+    private IEnumerator FadeOutImage()
+    {
+        yield return new WaitForSeconds(timeBeforeFade);
+
+        this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/FadeOutTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOutTitle : MonoBehaviour
+{
+    float fadeSpeed = 0.01f;
+    public Image titlePanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CutsceneManager.isFirstSpacebarHit == false)
+        {
+            float newAlpha = titlePanel.color.a - fadeSpeed;
+
+            titlePanel.color = new Color(titlePanel.color.r, titlePanel.color.g, titlePanel.color.b, newAlpha);
+        }
+    }
+}
